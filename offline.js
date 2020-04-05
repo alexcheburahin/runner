3a4ea2ea771a30eb67a29923a87494c4f21d4e40
alexcheburahin <63163338+alexcheburahin@users.noreply.github.com>
2020-04-05 20:44:21 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1586100765,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
